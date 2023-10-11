e98610f694b8bcfa3759916374efdb780ed12f9c
Samuel Idowu Ogunbodede <samiwogdev@gmail.com>
2023-10-11 14:19:00 +0100

    update Admin middleware

--- app/Http/Middleware/Admin.php
+++ app/Http/Middleware/Admin.php
@@ -16,8 +16,8 @@ class Admin
      */
     public function handle(Request $request, Closure $next): Response
     {
-        if(Auth::guard('admin')->check()){
-            return redirect('/admin/login');
+        if(!Auth::guard('admin')->check()){
+             return redirect('/admin/login');
         }
         return $next($request);
     }
--- routes/web.php
+++ routes/web.php
@@ -44,7 +44,9 @@ Route::prefix('/admin')->namespace('App\Http\Controllers\Admin')->group(function
 
     //Admin Login Route
     Route::match(['get', 'post'],'login', 'AdminController@login');
-
-    //Admin Dashboard Route
-    Route::get('dashboard', 'AdminController@dashboard');
+    //Add admin middleware
+    Route::group(['middleware' => ['admin']], function(){
+        //Admin Dashboard Route
+        Route::get('dashboard', 'AdminController@dashboard');
+    });
     });
