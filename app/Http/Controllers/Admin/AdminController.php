2e6c57d7b97d3e0f7fc6521d4d692dc4bec1e2af
Samuel Idowu Ogunbodede <samiwogdev@gmail.com>
2023-10-24 11:20:03 +0100

    load login, logout and Admin controller

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -4,14 +4,30 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Auth;
 
 class AdminController extends Controller
 {
-    public function dashboard(){
+    public function dashboard()
+    {
         return view('admin.dashboard');
     }
 
-    public function login(){
+    public function login(Request $request)
+    {
+        if ($request->isMethod('post')) {
+            $data = $request->all();
+            if (Auth::guard('admin')->attempt(['email' => $data["email"], 'password' => $data["password"], 'status' => 1])) {
+                return redirect('admin/dashboard');
+            } else {
+                return redirect()->back()->with("error_message", "Invalid email or password");
+            }
+        }
         return view('admin.login');
+    }
+
+    public function logout(){
+        Auth::guard('admin')->logout();
+        return redirect('admin/logout');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-// use App\Http\Controllers\Admin\AdminController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -37,16 +36,19 @@ require __DIR__.'/auth.php';
 
 
 
-
+// Route::get('admin/dashboard', 'AdminController@index')->name('admin.dashboard');
 
 
 Route::prefix('/admin')->namespace('App\Http\Controllers\Admin')->group(function(){
 
     //Admin Login Route
     Route::match(['get', 'post'],'login', 'AdminController@login');
+
     //Add admin middleware
     Route::group(['middleware' => ['admin']], function(){
+
         //Admin Dashboard Route
         Route::get('dashboard', 'AdminController@dashboard');
+        Route::get('logout', 'AdminController@logout');
     });
     });
